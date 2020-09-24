0c1a423c3c4e5a2aa1ed6eb1adea21cf5a3899b5
Michael Easter <codetojoy@gmail.com>
2020-09-24 12:59:24 -0300

    refactoring

--- src/main/java/com/example/CaseInfo.java
+++ src/main/java/com/example/CaseInfo.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import java.util.concurrent.atomic.AtomicInteger;
+// import java.util.concurrent.atomic.AtomicInteger;
 
 public class CaseInfo {
     public String caseId;
@@ -9,32 +9,45 @@ public class CaseInfo {
     public String c;
     public String d;
 
+    /*
     private static AtomicInteger counter = new AtomicInteger(0);
 
     public CaseInfo() {
         int value = counter.incrementAndGet();
         System.out.println("TRACER CaseInfo ctor counter: " + value);
     }
+    */
+    public void merge(CaseInfo partialCaseInfo) {
+        if (this.caseId == null || partialCaseInfo.caseId == null
+                || (! this.caseId.equals(partialCaseInfo.caseId))) {
+            throw new IllegalArgumentException("internal error on merge");
+        }
 
-    public static CaseInfo parseStr(String s) {
-        String[] tokens = s.split("|");
+        if (partialCaseInfo.a != null) { this.a = partialCaseInfo.a; }
+        if (partialCaseInfo.b != null) { this.b = partialCaseInfo.b; }
+        if (partialCaseInfo.c != null) { this.c = partialCaseInfo.c; }
+        if (partialCaseInfo.d != null) { this.d = partialCaseInfo.d; }
+    }
 
-        String caseId = tokens[0];
-        String a = tokens[1];
-        String b = tokens[2];
-        String c = tokens[3];
-        String d = tokens[4];
-
+    public static CaseInfo buildPartialCaseInfo(String caseId, String payload) {
         CaseInfo caseInfo = new CaseInfo();
         caseInfo.caseId = caseId;
-        caseInfo.a = a;
-        caseInfo.b = b;
-        caseInfo.c = c;
-        caseInfo.d = d;
+
+        String[] payloadTokens = payload.split(Constants.TOKEN_SEPARATOR);
+
+        int index = 0;
+        for (String payloadToken : payloadTokens) {
+            if ((payloadToken != null) && (! payloadToken.isEmpty())) {
+                if (index == 0) { caseInfo.a = payloadToken; }
+                if (index == 1) { caseInfo.b = payloadToken; }
+                if (index == 2) { caseInfo.c = payloadToken; }
+                if (index == 3) { caseInfo.d = payloadToken; }
+            }
+            index++;
+        }
 
         return caseInfo;
     }
-
 
     public String toString() {
         String result = String.format(Constants.CSV_OUTPUT_FORMAT, caseId, a, b, c, d);
--- src/main/java/com/example/Greeter.java
+++ src/main/java/com/example/Greeter.java
@@ -3,52 +3,42 @@ package com.example;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.*;
 
-import java.util.concurrent.atomic.AtomicInteger;
+// import java.util.concurrent.atomic.AtomicInteger;
 
 import com.example.message.*;
 
 public class Greeter extends AbstractBehavior<Greet> {
-  private CaseInfo caseInfo = new CaseInfo();
-  private AtomicInteger messageCounter = new AtomicInteger(0);
+    private CaseInfo caseInfo = new CaseInfo();
+    // private AtomicInteger messageCounter = new AtomicInteger(0);
 
-  public static Behavior<Greet> create() {
-    return Behaviors.setup(Greeter::new);
-  }
-
-  private Greeter(ActorContext<Greet> context) {
-    super(context);
-  }
-
-  @Override
-  public Receive<Greet> createReceive() {
-    return newReceiveBuilder().onMessage(Greet.class, this::onGreet).build();
-  }
-
-  private Behavior<Greet> onGreet(Greet command) {
-    int value = messageCounter.incrementAndGet();
-
-    if (command.isDone) {
-        getContext().getLog().info("TRACER Greeter DONE msg: {} case: {}", value, caseInfo.toString());
-        String caseInfoStr = caseInfo.toString();
-        command.replyTo.tell(new Greeted(caseInfoStr, command.whom, getContext().getSelf()));
-    } else {
-        if (caseInfo.caseId == null) { caseInfo.caseId = command.caseId; }
-
-        String[] payloadTokens = command.payload.split(Constants.TOKEN_SEPARATOR);
-        int index = 0;
-        for (String payloadToken : payloadTokens) {
-            if ((payloadToken != null) && (! payloadToken.isEmpty())) {
-                if (index == 0) { caseInfo.a = payloadToken; }
-                if (index == 1) { caseInfo.b = payloadToken; }
-                if (index == 2) { caseInfo.c = payloadToken; }
-                if (index == 3) { caseInfo.d = payloadToken; }
-            }
-            index++;
-        }
-
-        getContext().getLog().info("TRACER Greeter msg: {} case: {}", value, caseInfo.toString());
+    public static Behavior<Greet> create() {
+        return Behaviors.setup(Greeter::new);
     }
 
-    return this;
-  }
+    private Greeter(ActorContext<Greet> context) {
+        super(context);
+    }
+
+    @Override
+    public Receive<Greet> createReceive() {
+        return newReceiveBuilder().onMessage(Greet.class, this::onGreet).build();
+    }
+
+    private Behavior<Greet> onGreet(Greet command) {
+        // int value = messageCounter.incrementAndGet();
+
+        if (command.isDone) {
+            getContext().getLog().info("TRACER Greeter DONE case: {}", caseInfo.toString());
+            String caseInfoStr = caseInfo.toString();
+            command.replyTo.tell(new Greeted(caseInfoStr, command.whom, getContext().getSelf()));
+        } else {
+            if (caseInfo.caseId == null) { caseInfo.caseId = command.caseId; }
+            CaseInfo partialCaseInfo = CaseInfo.buildPartialCaseInfo(caseInfo.caseId, command.payload);
+            caseInfo.merge(partialCaseInfo);
+
+            getContext().getLog().info("TRACER Greeter case: {}", caseInfo.toString());
+        }
+
+        return this;
+    }
 }
