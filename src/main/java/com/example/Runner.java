4d57e2da498aae727c5629bee3824a1592eb195d
Michael Easter <codetojoy@gmail.com>
2020-09-24 19:31:54 -0300

    simple file watcher as a sentinel

--- src/main/java/com/example/Runner.java
+++ src/main/java/com/example/Runner.java
@@ -1,42 +1,44 @@
 package com.example;
 
 import akka.actor.typed.ActorSystem;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
-import java.nio.file.*;
 
 import com.example.message.*;
 
 public class Runner {
+    private static String csvFilename;
+    private static  final int DELAY_IN_MILLIS = 1000;
+
     public static void main(String[] args) {
-        String csvFilename = args[0];
+        csvFilename = args[0];
         ActorSystem<SayHello> greeterMain = ActorSystem.create(GreeterMain.create(csvFilename), "helloakka");
         greeterMain.tell(new SayHello("Charles"));
 
         try {
-            // watchFileUntilQuiet();
-            // System.out.println("TRACER sleeping");
-            // Thread.sleep(2 * 1000);
-            System.out.println(">>> Press ENTER to exit <<<");
-            System.in.read();
+            watchCsvFileUntilQuiet();
         } catch (Exception ignored) {
         } finally {
             greeterMain.terminate();
         }
     }
 
-    // experimental
-    static void watchFileUntilQuiet() throws Exception {
-        WatchService watchService = FileSystems.getDefault().newWatchService();
-        Path path = Paths.get("/Users/measter/src/github/codetojoy/COVID-19_CSV_Mapper_Akka");
-        path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
-        boolean poll = true;
-        while (poll) {
-            WatchKey key = watchService.take();
-            for (WatchEvent<?> event : key.pollEvents()) {
-                System.out.println("TRACER Event kind : " + event.kind() + " - File : " + event.context());
+    static void watchCsvFileUntilQuiet() throws Exception {
+        File csvFile = new File(csvFilename);
+        long lastTimeStamp = csvFile.lastModified();
+
+        boolean isDone = false;
+
+        while (! isDone) {
+            try { Thread.sleep(DELAY_IN_MILLIS); } catch (Exception ex) {}
+            long timeStamp = csvFile.lastModified();
+
+            if (timeStamp == lastTimeStamp) {
+                isDone = true;
+            } else {
+                System.out.println("TRACER csv file still being modified");
+                lastTimeStamp = timeStamp;
             }
-            poll = key.reset();
         }
     }
 }
